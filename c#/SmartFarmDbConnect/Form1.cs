2f5e32a85706686f4f1fd003035a8ae060f1fb13
stine <microwavestine@gmail.com>
2024-01-05 14:48:00 +0900

    c# ssh connection to mysql success

--- c#/SmartFarmDbConnect/Form1.cs
+++ c#/SmartFarmDbConnect/Form1.cs
@@ -1,6 +1,8 @@
 ﻿
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
 using Renci.SshNet;
+using DotNetEnv;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,117 +19,87 @@ namespace SmartFarmDbConnect
 {
     public partial class Form1 : Form
     {
-        static string SshHostName = "127.0.0.1";
-        static string SshUserName = "vagrant";
-        static string SshKeyFile = @"C:/Users/code/dbworks/.vagrant/machines/dbworks-01/virtualbox/private_key";
+        public static Tuple<SshClient, uint> ConnectSsh(string sshHostName, string sshUserName, string sshPassword = null,
+            string sshKeyFile = null, string sshPassPhrase = null, int sshPort = 2222, string databaseServer = "localhost", int databasePort = 3306)
+        {
+            // check arguments
+            if (string.IsNullOrEmpty(sshHostName))
+                throw new ArgumentException($"{nameof(sshHostName)} must be specified.", nameof(sshHostName));
+            if (string.IsNullOrEmpty(sshHostName))
+                throw new ArgumentException($"{nameof(sshUserName)} must be specified.", nameof(sshUserName));
+            if (string.IsNullOrEmpty(sshPassword) && string.IsNullOrEmpty(sshKeyFile))
+                throw new ArgumentException($"One of {nameof(sshPassword)} and {nameof(sshKeyFile)} must be specified.");
+            if (string.IsNullOrEmpty(databaseServer))
+                throw new ArgumentException($"{nameof(databaseServer)} must be specified.", nameof(databaseServer));
 
-        static string Server = "***.eu-west-1.rds.amazonaws.com";
-        static uint Port = 3306;
-        static string UserID = "root";
-        static string Password = "***";
-/*        static string DataBase = "***";*/
+            // define the authentication methods to use (in order)
+            var authenticationMethods = new List<AuthenticationMethod>();
+            if (!string.IsNullOrEmpty(sshKeyFile))
+            {
+                authenticationMethods.Add(new PrivateKeyAuthenticationMethod(sshUserName,
+                    new PrivateKeyFile(sshKeyFile, string.IsNullOrEmpty(sshPassPhrase) ? null : sshPassPhrase)));
+            }
+            if (!string.IsNullOrEmpty(sshPassword))
+            {
+                authenticationMethods.Add(new PasswordAuthenticationMethod(sshUserName, sshPassword));
+            }
+
+            // connect to the SSH server
+            var sshClient = new SshClient(new ConnectionInfo(sshHostName, sshPort, sshUserName, authenticationMethods.ToArray()));
+            sshClient.Connect();
+
+            // forward a local port to the database server and port, using the SSH server
+            var forwardedPort = new ForwardedPortLocal("127.0.0.1", databaseServer, (uint)databasePort);
+            sshClient.AddForwardedPort(forwardedPort);
+            forwardedPort.Start();
+
+            return new Tuple<SshClient, uint>(sshClient, forwardedPort.BoundPort);
+        }
 
         public Form1()
         {
+            DotNetEnv.Env.TraversePath().Load();
             InitializeComponent();
-
-            ConnectionInfo cnnInfo;
-            using (var stream = new FileStream(SshKeyFile, FileMode.Open, FileAccess.Read))
-            {
-                var file = new PrivateKeyFile(stream);
-                var authMethod = new PrivateKeyAuthenticationMethod(SshUserName, file);
-                cnnInfo = new ConnectionInfo(SshHostName, 22, SshUserName, authMethod);
-            }
-
-            using (var client = new SshClient(cnnInfo))
-            {
-                client.Connect();
-                if (client.IsConnected)
-                {
-                    var forwardedPort = new ForwardedPortLocal("127.0.0.1", Server, Port);
-                    client.AddForwardedPort(forwardedPort);
-                    forwardedPort.Start();
-
-                    string connStr = $"Server = {forwardedPort.BoundHost};Port = {forwardedPort.BoundPort};Uid = {UserID};Pwd = {Password};";
-
-                    using (MySqlConnection cnn = new MySqlConnection(connStr))
-                    {
-                        cnn.Open();
-
-                        MySqlCommand cmd = new MySqlCommand("SELECT * FROM PostalCodes LIMIT 25;", cnn);
-
-                        MySqlDataReader reader = cmd.ExecuteReader();
-
-                        while (reader.Read())
-                            Console.WriteLine($"{reader.GetString(1)}, {reader.GetString(2)}, {reader.GetString(3)}");
-
-                        Console.WriteLine("Ok");
-
-                        cnn.Close();
-                    }
-
-                    client.Disconnect();
-                }
-
-            }
-
-
-            /*            PasswordConnectionInfo sshConnInfo = new PasswordConnectionInfo("111.222.333.444", 22, "server-id", "server-pw"); 
-                        sshConnInfo.Timeout = TimeSpan.FromSeconds(30); 
-                        SshClient sshClient = new SshClient(sshConnInfo);
-
-                        try
-                        {
-                            sshClient.Connect();
-
-                            if (sshClient.IsConnected)
-                            {
-                                // tunnel job
-                                ForwardedPortLocal tunnel = new ForwardedPortLocal("127.0.0.1", 8008, "127.0.0.1", 3306);
-                                sshClient.AddForwardedPort(tunnel);
-                                tunnel.Start();
-
-                                // Mysql job
-                                MySqlConnection mycon = new MySqlConnection("Server=127.0.0.1;Database=db-name;Uid=db-id;Pwd=db-pw;SslMode=none;Port=8008;");
-                                mycon.Open();
-                                MySqlCommand mycmd = new MySqlCommand("sql statement");
-                                MySqlDataAdapter da = new MySqlDataAdapter();
-                                mycmd.Connection = mycon;
-                                da.SelectCommand = mycmd;
-                                DataTable dt = new DataTable();
-                                da.Fill(dt);
-                            }
-                            else
-                            {
-
-                            }
-
-                        } catch (Exception ex)
-                        {
-
-                        }*/
-
 
         }
 
+
+        public void InsertUpdate()
+        {
+            var server = "127.0.0.1";
+            var sshUserName = "vagrant";
+            var sshKeyFile = System.Environment.GetEnvironmentVariable("SSH_KEY_FILE");
+            var databaseUserName = System.Environment.GetEnvironmentVariable("DB_USERNAME");
+            var databasePassword = System.Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            Tuple<SshClient, uint> sshResult = ConnectSsh(server, sshUserName, null, sshKeyFile);
+            SshClient sshClient = sshResult.Item1;
+            uint localPort = sshResult.Item2;
+
+            using (sshClient)
+            {
+                MySqlConnectionStringBuilder csb = new MySqlConnectionStringBuilder
+                {
+                    Server = "127.0.0.1",
+                    Port = localPort,
+                    Database = "smartfarm",
+                    UserID = databaseUserName,
+                    Password = databasePassword,
+                };
+
+                using (MySqlConnection connection = new MySqlConnection(csb.ConnectionString))
+                {
+                    string date = DateTime.Now.ToString();
+                    connection.Open();
+                    MySqlCommand cmd = new MySqlCommand("INSERT INTO gp2y10(dust, date) VALUES('" + 23.234 + "', '" + date + "')", connection);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            InsertUpdate();
 
         }
     }
 }
-
-// credits to https://walkingfox.tistory.com/73
-
-/*Host dbworks-01
-  HostName 127.0.0.1
-  User vagrant
-  Port 2222
-  UserKnownHostsFile /dev/null
-  StrictHostKeyChecking no
-  PasswordAuthentication no
-  IdentityFile C:/ Users / code / dbworks /.vagrant / machines / dbworks - 01 / virtualbox / private_key
-  IdentitiesOnly yes
-  LogLevel FATAL
-  PubkeyAcceptedKeyTypes +ssh-rsa
-  HostKeyAlgorithms +ssh-rsa*/
