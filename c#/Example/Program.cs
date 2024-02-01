38ffa85a10eaca939d0bd3dce5aece5ea31da026
stine <microwavestine@gmail.com>
2024-02-01 16:44:19 +0900

    jfet

--- c#/Example/Program.cs
+++ c#/Example/Program.cs
@@ -1,44 +1,32 @@
-﻿// A very simple event demonstration.
+﻿// Demonstrate the Counter namespace.
 using System;
-// Declare a delegate type for an event.
-delegate void MyEventHandler();
-// Declare a class that contains an event.
-class MyEvent
+class NSDemo
 {
-    // public MyEventHandler SomeEvent;
-    public event MyEventHandler SomeEvent;
-    // This is called to raise the event.
-    public void OnSomeEvent()
-    {
-        if (SomeEvent != null)
-            SomeEvent();
-    }
-}
-class EventDemo
-{
-    // An event handler.
-    static void Handler()
-    {
-        Console.WriteLine("Event occurred");
-    }
-
-    static void Handler2()
-    {
-        Console.WriteLine("Event occured2");
-    }
-
-    static void Handler3()
-    {
-        Console.WriteLine("Event occured3");
-    }
-
     static void Main()
     {
-        MyEvent evt = new MyEvent();
-        // Add Handler() to the event list.
-        evt.SomeEvent += Handler;
-        // Raise the event.
-        //evt.SomeEvent();
-        evt.OnSomeEvent();
+        // Notice how CountDown is qualified by Counter.
+        Counter.CountDown cd1 = new Counter.CountDown(10);
+        int i;
+        do
+        {
+            i = cd1.Count();
+            Console.Write(i + " ");
+        } while (i > 0);
+        Console.WriteLine();
+        // Again, notice how CountDown is qualified by Counter.
+        Counter.CountDown cd2 = new Counter.CountDown(20);
+        do
+        {
+            i = cd2.Count();
+            Console.Write(i + " ");
+        } while (i > 0);
+        Console.WriteLine();
+        cd2.Reset(4);
+        do
+        {
+            i = cd2.Count();
+            Console.Write(i + " ");
+        } while (i > 0);
+        Console.WriteLine();
     }
 }
